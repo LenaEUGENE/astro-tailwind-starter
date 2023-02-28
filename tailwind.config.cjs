74090452460b265fb00dd3343b2054e741857f35
Lena EUGENE <lenaeugene@gmail.com>
2023-02-28 17:16:01 +0100

    Normalised css + added basic design system + font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      inter: "Inter var, sans-serif",
+    },
+    extend: {
+      colors: {
+        brand: "#F9A8D4",
+        accent: "#C084FC",
+        dark: "#1F2937",
+        offWhite: "#FCF8F1",
+        white: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
